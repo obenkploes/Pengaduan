a199ab1700343a7f6029e41f65a0d37974d8904e
jokokristianto <jokokristianto23@guru.smk.belajar.id>
2023-10-06 06:46:41 +0700

    add bootstrap

---
+++ resources/views/Layout.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') | Pengaduan</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/layout/css/style.css">
+</head>
+
+<body>
+    <div id="main" class="d-flex vh-100">
+        <div id="sidebar" class="bg-light shadow">
+            <nav class="navbar navbar-dark bg-primary bg-gradient px-3">
+                <span class="navbar-brand fw-bold">
+                    <i class="bi bi-phone-vibrate-fill"></i>
+                    Lapor!!
+                </span>
+            </nav>
+            <div id="menu" class="mt-4 ">
+                <ul class="nav flex-column ">
+                    <li class="nav-item mb-1">
+                        <a href="#" class="nav-link active">
+                            <i class="bi bi-house-fill"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item mb-1">
+                        <a href="#" class="nav-link ">Dashboard</a>
+                    </li>
+                    <li class="nav-item mb-1">
+                        <a href="#" class="nav-link ">Dashboard</a>
+                    </li>
+                    <li class="nav-item mb-1">
+                        <a href="#" class="nav-link ">Dashboard</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="center">
+            <nav class="navbar navbar-dark bg-primary bg-gradient shadow-sm">
+                <div class="container-fluid">
+                    <span class="navbar-brand">Dashboard</span>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
