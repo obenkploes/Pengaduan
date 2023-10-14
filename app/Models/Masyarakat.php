d99af5800eb46f70e3548e2aadeb08772efba49b
jokokristianto <jokokristianto23@guru.smk.belajar.id>
2023-10-14 11:35:04 +0700

    set model

--- app/Models/Masyarakat.php
+++ app/Models/Masyarakat.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Otentikasi;
 
-class Masyarakat extends Model
+class Masyarakat extends Otentikasi
 {
+
     use HasFactory;
+
+    protected $primaryKey='nik';
+    protected $keyType='string';
+
+    protected $incrementing=false;
+
+    public $guarded=[];
 }
--- app/Models/Pengaduan.php
+++ app/Models/Pengaduan.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Pengaduan extends Model
 {
     use HasFactory;
+    public $guarded=[];
 }
