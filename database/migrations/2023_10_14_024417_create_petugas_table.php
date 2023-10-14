fe4c2a6586b44d62515b58ff246ae8744fd60d07
jokokristianto <jokokristianto23@guru.smk.belajar.id>
2023-10-14 09:50:40 +0700

    add tabel petugas

---
+++ database/migrations/2023_10_14_024417_create_petugas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('petugas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_petugas',35)->nullable();
+            $table->string('username',25)->nullable();
+            $table->string('password',32)->nullable();
+            $table->string('telp',13)->nullable();
+            $table->enum('level',['admin','petugas'])->default('admin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('petugas');
+    }
+};
